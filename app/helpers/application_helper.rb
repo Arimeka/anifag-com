0ddc9470ca67a52ac1f667605e29e62ff68aa25b
Navi Arimeka <wrk.navi@gmail.com>
2015-08-01 15:20:10 +0300

    add social sharing meta-tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def active_action?(name, action)
     'active' if controller_name == name && action_name == action
   end
+
+  def get_current_path
+    request.original_fullpath.force_encoding('UTF-8').split('?')[0] || ''
+  end
+
+  def get_current_url
+    URI.join(root_url, URI.escape(get_current_path)).to_s
+  end
 end
